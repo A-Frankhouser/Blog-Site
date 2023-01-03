84e19147b0f8c2afcc0ed0b50bfea46ddcd9c850
Alex Frankhouser <freesoul423@gmail.com>
2023-01-03 18:01:59 -0500

    added main and index handlebars and started setting up the server

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-console.log(
-    `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
-);
+// Sets app to use handlebars engine
+app.set('view engine', 'handlebars');
+
+// Handlebar config
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+}));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', {layout : 'main' });
+})
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
