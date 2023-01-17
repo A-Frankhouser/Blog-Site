15d0196e851cbf1facbfd8af25608b06d1fe9c8a
Alex Frankhouser <freesoul423@gmail.com>
2023-01-16 19:35:33 -0500

    Created associatiations for db

--- models/User.js
+++ models/User.js
@@ -11,29 +11,29 @@ class User extends Model {
 User.init(
     {
         id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
     },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
     },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-        isEmail: true,
-        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+            isEmail: true,
+            },
     },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-        len: [8],
-        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        // validate: {
+        // len: [8],
+        // },
     },
 },
 {
@@ -50,11 +50,11 @@ User.init(
         return updatedUserData;
     },
     },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "user",
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
     }
 );
 
